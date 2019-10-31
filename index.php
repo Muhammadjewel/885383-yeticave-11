42f8249d1be4da760b028ad5577a73bd277e593a
muhammadjewel <msuratov@gmail.com>
2019-11-01 00:44:34 +0500

    Format lots' price

--- index.php
+++ index.php
@@ -41,6 +41,13 @@ $lots = [
         'image_url' => 'img/lot-6.jpg'
     ]
 ];
+function formatLotPrice ($price) {
+    $price = ceil($price);
+    if ($price >= 1000) {
+        $price = number_format($price, 0, '.', ' ');
+    }
+    return $price . ' ₽';
+}
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -115,7 +122,7 @@ $lots = [
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$lot['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=formatLotPrice($lot['price']); ?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
